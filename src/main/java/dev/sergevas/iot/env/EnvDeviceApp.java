fa99ff8a15dca0c14ee53ad38715e5cfeb92b202
sergevas <brighterr@gmail.com>
2023-09-26 23:46:29 +0300

    In progress

--- src/main/java/dev/sergevas/iot/env/EnvDeviceApp.java
+++ src/main/java/dev/sergevas/iot/env/EnvDeviceApp.java
@@ -1,37 +1,26 @@
 package dev.sergevas.iot.env;
 
 
-import cloud.piranha.embedded.EmbeddedPiranha;
-import cloud.piranha.embedded.EmbeddedPiranhaBuilder;
-import cloud.piranha.http.impl.DefaultHttpServer;
-import cloud.piranha.http.webapp.HttpWebApplicationServerProcessor;
-import dev.sergevas.iot.env.bh1750.adapter.in.http.Bh1750Servlet;
-import dev.sergevas.iot.env.system.adapter.in.http.SystemInfoServlet;
+import dev.sergevas.iot.env.bh1750.adapter.in.http.Bh1750Handler;
+import dev.sergevas.iot.env.system.adapter.in.http.SystemInfoHandler;
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.RoutingHandler;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.time.LocalDateTime;
-import java.util.logging.LogManager;
 
 //@ApplicationPath("/iotenv/api/v1")
 public class EnvDeviceApp {
 
-    static {
-        try (InputStream is = Thread.currentThread().getContextClassLoader().
-                getResourceAsStream("logging.properties")) {
-            LogManager.getLogManager().readConfiguration(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
-        EmbeddedPiranha piranha = new EmbeddedPiranhaBuilder()
-                .servletsMapped(SystemInfoServlet.class, "/system",
-                        Bh1750Servlet.class, "/bh1750")
-                .buildAndStart();
-        DefaultHttpServer server = new DefaultHttpServer(8080,
-                new HttpWebApplicationServerProcessor(piranha), false);
+        EnvDeviceAppConfig config = EnvDeviceAppServiceManager.getInstance().getEnvDeviceAppConfig();
+        final HttpHandler routes = new RoutingHandler()
+                .get("/bh1750", new Bh1750Handler())
+                .get("/system", new SystemInfoHandler());
+        Undertow server = Undertow.builder()
+                .addHttpListener(config.getHttpPort(), "0.0.0.0")
+                .setHandler(routes)
+                .build();
         System.out.println(LocalDateTime.now() + " Start server...");
         server.start();
         System.out.println(LocalDateTime.now() + " Start server complete...");
--- src/main/java/dev/sergevas/iot/env/bh1750/adapter/in/http/Bh1750Handler.java
+++ src/main/java/dev/sergevas/iot/env/bh1750/adapter/in/http/Bh1750Handler.java
@@ -5,35 +5,26 @@ import dev.sergevas.iot.env.bh1750.application.port.in.Bh1750UseCase;
 import dev.sergevas.iot.env.shared.entity.SensorName;
 import dev.sergevas.iot.env.shared.entity.SensorReadingsItemType;
 import dev.sergevas.iot.env.shared.entity.SensorType;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 
-public class Bh1750Servlet extends HttpServlet {
+public class Bh1750Handler implements HttpHandler {
 
-    private Bh1750UseCase bh1750UseCase;
-
-    @Override
-    public void init() {
-        bh1750UseCase = EnvDeviceAppServiceManager.getInstance().getBh1750UseCase();
-    }
+    private final Bh1750UseCase bh1750UseCase = EnvDeviceAppServiceManager.getInstance().getBh1750UseCase();
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
         SensorReadingsItemType sensorReadingsType = new SensorReadingsItemType()
                 .sType(SensorType.LIGHT.name())
                 .sName(SensorName.BH1750.getName())
                 .sTimestamp(OffsetDateTime.now(ZoneOffset.UTC))
                 .sData(String.valueOf(bh1750UseCase.getSensorReadingsItemTypeForBh1750().getLightIntensity()));
         System.out.printf("SensorReadingsType with Bh1750 data: %s", sensorReadingsType);
-        try (PrintWriter writer = response.getWriter()) {
-            writer.println(sensorReadingsType.toString());
-            writer.flush();
-        }
+        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+        exchange.getResponseSender().send(sensorReadingsType + "\n");
     }
 }
