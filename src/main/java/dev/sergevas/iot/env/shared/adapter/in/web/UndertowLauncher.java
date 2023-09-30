fed608185736dc6705fbc82d953bf8787d4f5c82
sergevas <brighterr@gmail.com>
2023-09-30 15:49:18 +0300

    In progress

--- src/main/java/dev/sergevas/iot/env/shared/adapter/in/web/UndertowLauncher.java
+++ src/main/java/dev/sergevas/iot/env/shared/adapter/in/web/UndertowLauncher.java
@@ -7,6 +7,8 @@ import dev.sergevas.iot.env.system.adapter.in.http.SystemInfoHandler;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.RoutingHandler;
+import io.undertow.server.handlers.accesslog.AccessLogHandler;
+import io.undertow.server.handlers.accesslog.JBossLoggingAccessLogReceiver;
 
 import java.time.LocalDateTime;
 
@@ -16,12 +18,17 @@ public class UndertowLauncher {
 
     public static Undertow startServer() {
         EnvDeviceAppConfig config = EnvDeviceAppServiceManager.getInstance().getEnvDeviceAppConfig();
+
         final HttpHandler routes = new RoutingHandler()
                 .get("/bh1750", new Bh1750Handler())
                 .get("/system", new SystemInfoHandler());
+
+        AccessLogHandler accessLogHandler = new AccessLogHandler(routes,
+                new JBossLoggingAccessLogReceiver(), "combined", UndertowLauncher.class.getClassLoader());
+
         Undertow server = Undertow.builder()
                 .addHttpListener(config.getHttpPort(), "0.0.0.0")
-                .setHandler(routes)
+                .setHandler(accessLogHandler)
                 .build();
         System.out.println(LocalDateTime.now() + " Start server...");
         server.start();
