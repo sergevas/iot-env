a64247e0aa8a77f0d832e3842a61755cd004a3af
sergevas <brighterr@gmail.com>
2023-09-30 10:24:26 +0300

    Move app server starter code from main

--- src/main/java/dev/sergevas/iot/env/EnvDeviceApp.java
+++ src/main/java/dev/sergevas/iot/env/EnvDeviceApp.java
@@ -1,28 +1,11 @@
 package dev.sergevas.iot.env;
 
 
-import dev.sergevas.iot.env.bh1750.adapter.in.http.Bh1750Handler;
-import dev.sergevas.iot.env.system.adapter.in.http.SystemInfoHandler;
-import io.undertow.Undertow;
-import io.undertow.server.HttpHandler;
-import io.undertow.server.RoutingHandler;
+import dev.sergevas.iot.env.shared.adapter.in.web.UndertowLauncher;
 
-import java.time.LocalDateTime;
-
-//@ApplicationPath("/iotenv/api/v1")
 public class EnvDeviceApp {
 
     public static void main(String[] args) {
-        EnvDeviceAppConfig config = EnvDeviceAppServiceManager.getInstance().getEnvDeviceAppConfig();
-        final HttpHandler routes = new RoutingHandler()
-                .get("/bh1750", new Bh1750Handler())
-                .get("/system", new SystemInfoHandler());
-        Undertow server = Undertow.builder()
-                .addHttpListener(config.getHttpPort(), "0.0.0.0")
-                .setHandler(routes)
-                .build();
-        System.out.println(LocalDateTime.now() + " Start server...");
-        server.start();
-        System.out.println(LocalDateTime.now() + " Start server complete...");
+        UndertowLauncher.startServer();
     }
 }
---
+++ src/main/java/dev/sergevas/iot/env/shared/adapter/in/web/UndertowLauncher.java
@@ -0,0 +1,31 @@
+package dev.sergevas.iot.env.shared.adapter.in.web;
+
+import dev.sergevas.iot.env.EnvDeviceAppConfig;
+import dev.sergevas.iot.env.EnvDeviceAppServiceManager;
+import dev.sergevas.iot.env.bh1750.adapter.in.http.Bh1750Handler;
+import dev.sergevas.iot.env.system.adapter.in.http.SystemInfoHandler;
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.RoutingHandler;
+
+import java.time.LocalDateTime;
+
+//@ApplicationPath("/iotenv/api/v1")
+
+public class UndertowLauncher {
+
+    public static Undertow startServer() {
+        EnvDeviceAppConfig config = EnvDeviceAppServiceManager.getInstance().getEnvDeviceAppConfig();
+        final HttpHandler routes = new RoutingHandler()
+                .get("/bh1750", new Bh1750Handler())
+                .get("/system", new SystemInfoHandler());
+        Undertow server = Undertow.builder()
+                .addHttpListener(config.getHttpPort(), "0.0.0.0")
+                .setHandler(routes)
+                .build();
+        System.out.println(LocalDateTime.now() + " Start server...");
+        server.start();
+        System.out.println(LocalDateTime.now() + " Start server complete...");
+        return server;
+    }
+}
