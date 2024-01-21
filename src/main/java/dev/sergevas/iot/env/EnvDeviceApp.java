5c1f23b77ceb9b5e913b6c087b49d07940bca51c
sergevas <brighterr@gmail.com>
2024-01-22 00:27:03 +0300

    Migrate to Piranha v24.1.0

--- src/main/java/dev/sergevas/iot/env/EnvDeviceApp.java
+++ src/main/java/dev/sergevas/iot/env/EnvDeviceApp.java
@@ -1,10 +1,9 @@
 package dev.sergevas.iot.env;
 
-
 import cloud.piranha.embedded.EmbeddedPiranha;
 import cloud.piranha.embedded.EmbeddedPiranhaBuilder;
 import cloud.piranha.http.impl.DefaultHttpServer;
-import cloud.piranha.http.webapp.HttpWebApplicationServerProcessor;
+import cloud.piranha.http.webapp.HttpWebApplicationServer;
 import dev.sergevas.iot.env.bh1750.adapter.in.http.Bh1750Servlet;
 import dev.sergevas.iot.env.system.adapter.in.http.SystemInfoServlet;
 
@@ -17,8 +16,8 @@ import java.util.logging.LogManager;
 public class EnvDeviceApp {
 
     static {
-        try (InputStream is = Thread.currentThread().getContextClassLoader().
-                getResourceAsStream("logging.properties")) {
+        try (InputStream is = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("logging.properties")) {
             LogManager.getLogManager().readConfiguration(is);
         } catch (IOException e) {
             e.printStackTrace();
@@ -30,8 +29,9 @@ public class EnvDeviceApp {
                 .servletsMapped(SystemInfoServlet.class, "/system",
                         Bh1750Servlet.class, "/bh1750")
                 .buildAndStart();
-        DefaultHttpServer server = new DefaultHttpServer(8080,
-                new HttpWebApplicationServerProcessor(piranha), false);
+        HttpWebApplicationServer webAppServer = new HttpWebApplicationServer();
+        webAppServer.addWebApplication(piranha.getWebApplication());
+        DefaultHttpServer server = new DefaultHttpServer(8080, webAppServer, false);
         System.out.println(LocalDateTime.now() + " Start server...");
         server.start();
         System.out.println(LocalDateTime.now() + " Start server complete...");
